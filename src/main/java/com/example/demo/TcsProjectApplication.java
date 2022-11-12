bb8635358255737de083ce32ef30325c81732fd1
unknown <khambalkarlavish@gmail.com>
2022-11-12 08:21:51 +0530

    Done

---
+++ src/main/java/com/example/demo/TcsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcsProjectApplication.class, args);
+	}
+
+}
